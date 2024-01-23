834b3772cd21d45be808aed97fee54ed85660f2c
Gabriel-Dias <gabrieldias_02@hotmail.com>
2024-01-23 18:51:40 -0300

    section inicio finalizada

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-// function infoAnimaisAberto() {
-//     let mostrarInfo = document.querySelector(".infoIconAnimais");
-//     if (mostrarInfo.style.opacity == 0) {
-//         mostrarInfo.style.opacity = "100%";
-//     } else {
-//         mostrarInfo.style.opacity = "0%";
-//     }
-// }
+
+//  function infoAnimaisAberto() {
+//      let mostrarInfo = document.querySelectorAll(".infoIconAnimais");
+//     mostrarInfo.style.opacity = "100%"
+//  }
