5b8a308eeafecc3b2b35355471010e3d898e89d3
Gabriel-Dias <gabrieldias_02@hotmail.com>
2024-01-31 17:19:47 -0300

    att carrosel

---
+++ paginasExtras/carrosselAjude.js
@@ -0,0 +1,8 @@
+
+function proxima(){
+    let container1 = document.querySelector(".container-card1");
+    let container2 = document.querySelector('.container-card2')
+    container2.style.right = "0%";
+    container1.style.left = "-100%";
+
+}
