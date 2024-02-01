e24f51a60f575d2b15443300ca9bd42b96286720
Gabriel-Dias <gabrieldias_02@hotmail.com>
2024-02-01 17:13:16 -0300

    att carrosel

--- paginasExtras/carrosselAjude.js
+++ paginasExtras/carrosselAjude.js
@@ -1,8 +1,30 @@
 
 function proxima(){
-    let container1 = document.querySelector(".container-card1");
-    let container2 = document.querySelector('.container-card2')
+    let container1 = document.querySelector('.container-card1');
+    let container2 = document.querySelector('.container-card2');
+    const btnProxima = document.querySelector('.proxima');
+    const btnAnterior = document.querySelector(".anterior");
+
+    btnProxima.style.color = "#a5c21387";
+    btnAnterior.style.color = "#a4c213";
+    btnProxima.disabled = true;
+    btnAnterior.disabled = false;
     container2.style.right = "0%";
     container1.style.left = "-100%";
 
+
+}
+
+function anterior() {
+  let container1 = document.querySelector(".container-card1");
+  let container2 = document.querySelector(".container-card2");
+  const btnProxima = document.querySelector(".proxima");
+  const btnAnterior = document.querySelector(".anterior");
+
+  btnProxima.style.color = "#a4c213";
+  btnAnterior.style.color = "#a5c21387";
+  btnProxima.disabled = false;
+  btnAnterior.disabled = true;
+  container2.style.right = "-100%";
+  container1.style.left = "0%";
 }
