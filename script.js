d08f79db67a3f5a8e3e33bb1f36469c3590ed82d
Gabriel-Dias <gabrieldias_02@hotmail.com>
2024-03-11 12:17:54 -0300

    att menu

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ let menu = document.querySelector(".menuTelasMenores");
 
 function openMenu() {
   menu.style.transform = "translateX(0%)";
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+  document.body.style.overflow = "hidden";
 }
 
 function closedMenu() {
-    menu.style.transform = "translateX(100%)";
+  menu.style.transform = "translateX(100%)";
+  document.body.style.overflow = "overlay";
 }
+
