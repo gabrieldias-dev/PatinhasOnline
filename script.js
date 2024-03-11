0d4c380a98ef6c0ac53e666c4afc4e5f7df6875a
Gabriel-Dias <gabrieldias_02@hotmail.com>
2024-03-11 11:50:34 -0300

    criação do menu hamburguer

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+let menu = document.querySelector(".menuTelasMenores");
 
+function openMenu() {
+  menu.style.transform = "translateX(0%)";
+}
+
+function closedMenu() {
+    menu.style.transform = "translateX(100%)";
+}
