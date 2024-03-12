3acb99037795ee48fb52b61e3709f3989174a978
Gabriel-Dias <gabrieldias_02@hotmail.com>
2024-03-12 10:53:54 -0300

    novos ajustes

--- script.js
+++ script.js
@@ -14,3 +14,15 @@ function closedMenu() {
   document.body.style.overflow = "overlay";
 }
 
+function ajustarSlidesPorViewport() {
+  let elemento = document.querySelector("#swiperAnimais");
+  if (window.innerWidth < 768) {
+    elemento.setAttribute("slides-per-view", "2");
+  } else {
+    elemento.setAttribute("slides-per-view", "3");
+  }
+}
+
+// Chamar a função quando a página carregar e quando a janela for redimensionada
+window.onload = ajustarSlidesPorViewport;
+window.addEventListener("resize", ajustarSlidesPorViewport);
