ddf6d13916c14173ec44940d42a7d980284a2a4a
Gabriel-Dias <gabrieldias_02@hotmail.com>
2024-03-15 11:26:02 -0300

    ajustes home e menu

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-let menu = document.querySelector(".menuTelasMenores");
+// let menu = document.querySelector(".menuTelasMenores");
 
-function openMenu() {
-  menu.style.transform = "translateX(0%)";
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
-  document.body.style.overflow = "hidden";
-}
+// function openMenu() {
+//   menu.style.transform = "translateX(0%)";
+//   window.scrollTo({
+//     top: 0,
+//     behavior: "smooth",
+//   });
+//   document.body.style.overflow = "hidden";
+// }
 
-function closedMenu() {
-  menu.style.transform = "translateX(100%)";
-  document.body.style.overflow = "overlay";
-}
+// function closedMenu() {
+//   menu.style.transform = "translateX(100%)";
+//   document.body.style.overflow = "overlay";
+// }
 
 function ajustarSlidesPorViewport() {
   let elemento = document.querySelector("#swiperAnimais");
