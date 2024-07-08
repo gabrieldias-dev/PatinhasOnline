186b33fed91a9c48c3cd401d7f66606f7c061aa8
Gabriel-Dias <gabrieldias_02@hotmail.com>
2024-07-08 17:50:58 -0300

    ajustes nav

--- main.js
+++ main.js
@@ -14,6 +14,10 @@
 //   document.body.style.overflow = "overlay";
 // }
 
+function desativado() {
+  window.alert ("Página dos Animais está em manutenção para ajustes e atualizações. Breve poderá conhecer todos nossos peludinhos (; ")
+}
+
 function ajustarSlidesPorViewport() {
   let carrosselAnimais = document.querySelector("#swiperAnimais");
   let historiasFelizes = document.querySelector("#swiperHistorias");
