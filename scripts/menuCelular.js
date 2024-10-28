5ec854fa29990c1c763cc5cab1246df7ffaab37e
Gabriel-Dias <gabrieldias_02@hotmail.com>
2024-10-28 09:48:25 -0300

    corrigido bug botão inicial

--- scripts/menuCelular.js
+++ scripts/menuCelular.js
@@ -1,4 +1,6 @@
 let menu = document.querySelector(".menuTelasMenores");
+let btnajuda = document.querySelector(".btn-ajude-causa");
+
 
 function openMenu() {
   menu.style.transform = "translateX(0%)";
@@ -7,9 +9,11 @@ function openMenu() {
     behavior: "smooth",
   });
   document.body.style.overflow = "hidden";
+  btnajuda.style.zIndex = "0";
 }
 
 function closedMenu() {
   menu.style.transform = "translateX(100%)";
   document.body.style.overflow = "overlay";
+  btnajuda.style.zIndex = "10";
 }
